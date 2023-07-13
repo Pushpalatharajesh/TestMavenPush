6fc558443883520f828e718427afbbd93db10e68
Pushpalatharajesh <pushpahebbar2@gmail.com>
2023-07-13 09:59:01 +0530

    Test

---
+++ MavenCommandPractice/src/test/java/Practice/Example1Test.java
@@ -0,0 +1,20 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Example1Test {
+
+	@Test
+	
+		public void CreateContact()
+		{
+			System.out.println("contact created");
+		}
+	@Test
+	public void ModifyContact()
+	{
+		System.out.println("contact created");
+
+	}
+}
+
---
+++ MavenCommandPractice/src/test/java/Practice/Example2Test.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Example2Test {
+@Test
+public void CreateName()
+{
+System.out.println("Pushpa");	
+	
+}
+public void CreateContact()
+{
+	System.out.println("974039");
+}
+}
